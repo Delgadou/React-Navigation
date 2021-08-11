101f164958b9b9bdc2a9248d7235320e079a236a
Henrique Delgado <delgado.ohenrique@gmail.com>
2021-08-11 13:54:52 -0300

    Add navigation with push

--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -3,13 +3,32 @@ import {createStackNavigator} from '@react-navigation/stack'
 import Screen0 from '../views/Screen0'
 import Screen1 from '../views/Screen1'
 import Screen2 from '../views/Screen2'
+import StepStack from '../components/StepStack'
 
 const Stack = createStackNavigator()
 
 export default props =>(
     <Stack.Navigator initialRouteName="Tela0">
-        <Stack.Screen name="Tela0" options={{title: 'Informações iniciais' }}component={Screen0}/>
-        <Stack.Screen name="Tela1" component={Screen1}/>
-        <Stack.Screen name="Tela2" component={Screen2}/>
+        <Stack.Screen name="Tela0" options={{title: 'Informações iniciais' }}>
+            {props => (
+                <StepStack {...props} next="Screen1">
+                    <Screen0/>
+                </StepStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen name="Screen1" >
+            {props => (
+                <StepStack {...props} back next="Screen2">
+                    <Screen1/>
+                </StepStack>
+            )}
+        </Stack.Screen>
+        <Stack.Screen name="Screen2">
+            {props => (
+                <StepStack {...props} back >
+                    <Screen2/>
+                </StepStack>
+            )}
+        </Stack.Screen>
     </Stack.Navigator>
 )
--- src/views/Screen0.js
+++ src/views/Screen0.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import CentralText from '../components/CentralText'
 
-export default props =>(
-    <CentralText backgroundColor='#E53935'>
-        Tela 0
-    </CentralText>
-)
+export default props => {
+    return(
+        <CentralText backgroundColor='#E53935'>
+            Tela 0
+        </CentralText>
+    )
+}
